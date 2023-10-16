57415e67d30d88be15a55bab7f6b9e87638aa13d
Marcell Pál <96981873+marcell114@users.noreply.github.com>
2023-10-16 14:57:30 +0200

    Add files via upload

---
+++ ft_tolower.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_tolower(char *str)
+{
+		int i;
+
+		i = 0;
+		while (str[i] != '\0')
+		{
+				if (str[i] <= 'Z' && str[i] >= 'A')
+				{
+						str[i] = str[i] + 32;
+				}
+				write (1, &str[i], 1);
+				i++;
+		}
+}
